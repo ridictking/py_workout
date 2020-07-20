0ffa5d8caee611099b375f826bacabe96ecb0fb1
Ridhwan Oladejo <Ridhwan.Oladejo@Interswitchng.com>
2020-07-20 17:28:49 +0100

    one liner for pig_latin and reverse_pig_latin

--- strings.py
+++ strings.py
@@ -14,7 +14,7 @@ class WordUtils:
             else:
                 word = f'{word[1:]}{word[0:1]}ay'
             collect.append(word)
-        print(' '.join(collect))
+        return ' '.join(collect)
 
     def pig_latin_v2(self,sentence):
         anot =' '.join(list(map(lambda word : f'{word}way' if word[0] in 'aeiouAEIOU' else f'{word[1:]}{word[0:1]}ay',sentence.split())))
@@ -34,21 +34,21 @@ class WordUtils:
         return reverse
  
 
-test = WordUtils()
-#test.pig_latin('This life is not an easy place to live in')
-words = """
-or the Sync   you need to have network , 
-subscription type=new or oneoff, partnerID 
-which we need to check if active or not, and for AsynC , 
-susbription type = renewal  and we need to check if 
-they have callback url that is working
-"""
-res = test.pig_latin_v2(words)
-print(res)
-print(test.reverse_pig_latin(res))
+# test = WordUtils()
+# #test.pig_latin('This life is not an easy place to live in')
+# words = """
+# or the Sync   you need to have network , 
+# subscription type=new or oneoff, partnerID 
+# which we need to check if active or not, and for AsynC , 
+# susbription type = renewal  and we need to check if 
+# they have callback url that is working
+# """
+# res = test.pig_latin_v2(words)
+# print(res)
+# print(test.reverse_pig_latin(res))
 
 
-x = lambda x,q : x[x.find(q)-18 : x.find(q)+18] if q in x else -1
+# x = lambda x,q : x[x.find(q)-18 : x.find(q)+18] if q in x else -1
 
 
 #print(x(words, 'check'))
